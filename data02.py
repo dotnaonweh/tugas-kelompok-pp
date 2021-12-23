5f27733b9da068de1498f30f952130f8ccef5d3f
Ichsan NA <49785290+dotnaonweh@users.noreply.github.com>
2021-12-23 19:33:10 +0700

    Update data02.py

--- data02.py
+++ data02.py
@@ -10,17 +10,3 @@ def main():
 	print(data)
 	print('Tipe data :',type(data))
 main()
-
-# 1
-
-# def baca_data02():
-# 	x = open('data02.txt','r')
-# 	return x
-
-# def main():
-# 	baca = baca_data02()
-# 	for i in baca:
-# 		data = list(map(int, i.split()))
-# 		print(data)
-# 	print('Tipe data :',type(data))
-# main()
